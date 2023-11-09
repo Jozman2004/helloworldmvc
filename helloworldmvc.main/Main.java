84651de89cfcc01bff3bcca4c611efa4d23af98a
Jozman2004 <joseph.nkoulou@2027.ucac-icam.com>
2023-11-09 11:41:49 +0100

    Commit 1

--- helloworldmvc.main/Main.java
+++ helloworldmvc.main/Main.java
@@ -1,12 +1,16 @@
+package fr.exia;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        // Point d'entrée de votre application.
-        // Vous pouvez instancier les objets nécessaires et lancer le contrôleur ici.
+    public static void main(final String[] args) throws IOException {
+        String file = "Helloworld.txt";
 
-        IView view = new YourView(); // Remplacez YourView par votre classe d'implémentation de l'interface IView.
-        IModel model = new YourModel(); // Remplacez YourModel par votre classe d'implémentation de l'interface IModel.
-
-        Controller controller = new Controller(view, model);
-        controller.run();
+        try {
+            String fileContent = DAOHelloWorld.readFile(file);
+            System.out.println(fileContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
